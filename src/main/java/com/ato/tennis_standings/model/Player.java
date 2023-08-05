6b1beb046de1c03f021a90edd03fdfb392338740
Jonathan Ato Markin <jona10711@gmail.com>
2023-08-05 22:22:00 +0200

    Player entity created

--- src/main/java/com/ato/tennis_standings/model/Player.java
+++ src/main/java/com/ato/tennis_standings/model/Player.java
@@ -1,3 +1,33 @@
 package com.ato.tennis_standings.model;
 
-public class Player {}
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "player")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "points")
+    private int points;
+
+    @Column(name = "games_played")
+    private int gamesPlayed;
+
+    @Column(name = "games_won")
+    private int gamesWon;
+
+    @Column(name = "games_lost")
+    private int gamesLost;
+}
