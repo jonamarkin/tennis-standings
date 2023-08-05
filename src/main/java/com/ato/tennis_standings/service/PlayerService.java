d25fec993b31d54f01a2747a4f4b118fef8005c4
Jonathan Ato Markin <jona10711@gmail.com>
2023-08-05 22:32:00 +0200

    Player Service completed

---
+++ src/main/java/com/ato/tennis_standings/service/PlayerService.java
@@ -0,0 +1,23 @@
+package com.ato.tennis_standings.service;
+
+import com.ato.tennis_standings.model.Player;
+import com.ato.tennis_standings.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlayerService {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    public List<Player> getAllPlayers() {
+        return playerRepository.findAll();
+    }
+
+    public Player getPlayerById(Long id) {
+        return playerRepository.findById(id).orElse(null);
+    }
+}
